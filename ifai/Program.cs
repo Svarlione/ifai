5561c5b529ae54603468e322d88d41e358542373
Svarlione <ruslan.brigockij@gmail.com>
2023-08-23 21:41:13 +0300

    Add project files.

---
+++ ifai/Program.cs
@@ -0,0 +1,9 @@
+﻿Console.WriteLine("Labas, iveskite skaiciu,aciu");
+string userInput = Console.ReadLine();
+int userInputInteger = Convert.ToInt32(userInput);
+if (userInputInteger > 100)
+ Console.WriteLine("Jusu skaicius yra didesnis nei 100");
+    else if (userInputInteger == 100)
+        Console.WriteLine("Skaicius yra lygus 100");
+    else
+        Console.WriteLine("skaicius yra mazesmis nei 100");
